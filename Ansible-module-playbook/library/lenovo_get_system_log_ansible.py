577fc0048d43b5c80863ffd191558a47469c029f
yuanjianshi <40456699+yuanjianshi@users.noreply.github.com>
2018-06-26 14:17:26 +0800

    sychronize with standard_api code

--- Ansible-module-playbook/library/lenovo_get_system_log_ansible.py
+++ Ansible-module-playbook/library/lenovo_get_system_log_ansible.py
@@ -60,15 +60,15 @@ def get_system_log(ip, login_account, login_password ):
     login_host = 'https://' + ip
     result = {}
     log_details = []
-    login_host = 'https://' + ip
     # Connect using the address, account name, and password
 
     ## Create a REDFISH object
-    REDFISH_OBJ = redfish.redfish_client(base_url=login_host, username=login_account,
-                                         password=login_password, default_prefix='/redfish/v1')
+
 
     # Login into the server and create a session
     try:
+        REDFISH_OBJ = redfish.redfish_client(base_url=login_host, username=login_account,
+                                         password=login_password, default_prefix='/redfish/v1')
         REDFISH_OBJ.login(auth="session")
     except:
         result = {'ret': False, 'msg': "Please check the username, password, IP is correct"}
