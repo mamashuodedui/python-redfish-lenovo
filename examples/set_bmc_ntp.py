224cbc5711cff922fca56efc801bf88001b86d34
Xiaofeng XF20 Liu <liuxf20@Lenovo.com>
2018-11-15 16:14:04 +0800

    Modified the set BMC ntp, Modified parameter type validation, Perfect exception handling

--- examples/set_bmc_ntp.py
+++ examples/set_bmc_ntp.py
@@ -27,7 +27,7 @@ import lenovo_utils as utils
 
 
 def set_manager_ntp(ip, login_account, login_password, ntp_server, ProtocolEnabled):
-    """Set Bios attribute    
+    """Set manager ntp server
     :params ip: BMC IP address
     :type ip: string
     :params login_account: BMC user name
@@ -54,51 +54,59 @@ def set_manager_ntp(ip, login_account, login_password, ntp_server, ProtocolEnabl
     except:
         result = {'ret': False, 'msg': "Please check the username, password, IP is correct\n"}
         return result
-    # Get ServiceBase resource
-    response_base_url = REDFISH_OBJ.get('/redfish/v1', None)
-    # Get response_base_url
-    if response_base_url.status == 200:
-        manager_url = response_base_url.dict['Managers']['@odata.id']
-    else:
-        result = {'ret': False, 'msg': " response_base_url Error code %s" % response_base_url.status}
-        REDFISH_OBJ.logout()
-        return result
-    response_manager_url = REDFISH_OBJ.get(manager_url, None)
-    if response_manager_url.status == 200:
-        for request in response_manager_url.dict['Members']:
-            request_url = request['@odata.id']
-            response_url = REDFISH_OBJ.get(request_url, None)
-            if response_url.status == 200:
-                network_url = response_url.dict['NetworkProtocol']['@odata.id']
-                Protocol = {"NTPServers":list(ntp_server),"ProtocolEnabled":  bool(int(ProtocolEnabled))}
-                parameter = {"NTP": Protocol}
-                response_network_url = REDFISH_OBJ.patch(network_url, body=parameter)
-                if response_network_url.status == 200:
-                    result = {'ret': True, 'msg': " Set manager ntp successful"}
+    try:
+        # Get ServiceBase resource
+        response_base_url = REDFISH_OBJ.get('/redfish/v1', None)
+        # Get response_base_url
+        if response_base_url.status == 200:
+            manager_url = response_base_url.dict['Managers']['@odata.id']
+        else:
+            error_message = utils.get_extended_error(response_base_url)
+            result = {'ret': False, 'msg': "Url '/redfish/v1' response error code %s \nerror_message: %s" % (
+                response_base_url.status, error_message)}
+            return result
+        response_manager_url = REDFISH_OBJ.get(manager_url, None)
+        if response_manager_url.status == 200:
+            for request in response_manager_url.dict['Members']:
+                request_url = request['@odata.id']
+                response_url = REDFISH_OBJ.get(request_url, None)
+                if response_url.status == 200:
+                    network_url = response_url.dict['NetworkProtocol']['@odata.id']
+                    Protocol = {"NTPServers":ntp_server,"ProtocolEnabled":  bool(int(ProtocolEnabled))}
+                    parameter = {"NTP": Protocol}
+                    response_network_url = REDFISH_OBJ.patch(network_url, body=parameter)
+                    if response_network_url.status == 200:
+                        result = {'ret': True, 'msg': " Set manager ntp servers successfully"}
+                    else:
+                        error_message = utils.get_extended_error(response_network_url)
+                        result = {'ret': False, 'msg': "Url '%s' response error code %s \nerror_message: %s" % (
+                            network_url, response_network_url.status, error_message)}
+                        return result
                 else:
-                    result = {'ret': False, 'msg': "response network Error code %s" % response_network_url.status}
-                    REDFISH_OBJ.logout()
+                    error_message = utils.get_extended_error(response_url)
+                    result = {'ret': False, 'msg': "Url '%s' response error code %s \nerror_message: %s" % (
+                        request_url, response_url.status, error_message)}
                     return result
-            else:
-                result = {'ret': False, 'msg': "response  url Error code %s" % response_url.status}
-                REDFISH_OBJ.logout()
-                return result
 
-    else:
-        result = {'ret': False, 'msg': "response manager url Error code %s" % response_manager_url.status}
+        else:
+            error_message = utils.get_extended_error(response_manager_url)
+            result = {'ret': False, 'msg': "Url '%s' response error code %s \nerror_message: %s" % (
+                manager_url, rresponse_manager_url.status, error_message)}
+            return result
+    except Exception as e:
+        result = {'ret': False, 'msg': "error message %s" % e}
+    finally:
+        # Logout of the current session
         REDFISH_OBJ.logout()
         return result
-
-    REDFISH_OBJ.logout()
-    return result
 
 
 import argparse
 def add_parameter():
     """Add set manager ntp parameter"""
     argget = utils.create_common_parameter_list()
-    argget.add_argument('--ntpserver', type=str, help='Input the ntp server(array  Items: string,Item count: 4)')
-    argget.add_argument('--protocol', type=str, help='Input the rotocolEnabled (0:false, 1:true)')
+    argget.add_argument('--ntpserver', nargs=4, type=str, help='Input the ntp server(array  Items: string,Item count: 4)')
+    argget.add_argument('--protocol', type=str, choices = ["0", "1"], help='Input the rotocolEnabled (0:false, 1:true)')
     args = argget.parse_args()
     parameter_info = utils.parse_parameter(args)
     return parameter_info
